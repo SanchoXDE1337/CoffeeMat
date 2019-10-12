917e1207f00e07adcb3c9695350ad69a8e0ab7d3
Alexander Dolgov <sanhoxd@gmail.com>
2019-10-13 02:05:07 +0300

    :sparkles:

--- index.js
+++ index.js
@@ -59,9 +59,13 @@ const volumes = {
 let cards = document.querySelectorAll('.card');
 const buttons = document.querySelectorAll(".button");
 const beauty = document.querySelector(".beauty");
+const beautyFaceBack = document.querySelector(".beauty__face--back");
 const coffeeName = document.querySelector("#coffeeName");
 const coffeePrice = document.querySelector("#coffeePrice");
 const coffeeVolume = document.querySelector("#coffeeVolume");
+const coffeeAdditiveSyrup = document.querySelector("#additiveSyrup");
+const coffeeAdditiveMilk = document.querySelector("#additiveMilk");
+const additiveChangers = document.querySelectorAll(".input-group");
 const milkMinusButton = document.querySelector(".button-minus.milk");
 const milkPlusButton = document.querySelector(".button-plus.milk");
 const milkQuantityField = document.querySelector(".quantity-field.milk");
@@ -102,13 +106,20 @@ cards.forEach(card => {
 buttons.forEach(button => {
     button.addEventListener('click', async function () {
         button.style.visibility = 'hidden';
+        let id = +clickedElem.id;
         buttonWasClicked = !buttonWasClicked;
         if (buttonWasClicked) {
-            console.log(clickedElem.id);
-            coffeeName.innerHTML = `Вы заказали: ${menu[clickedElem.id].name}`;
-            coffeeVolume.innerHTML = `Объем: ${menu[clickedElem.id].volume}мл`;
-            coffeePrice.innerHTML = `К оплате: ${menu[clickedElem.id].price}р`;
+            console.log(id);
+            console.log(additiveChangers);
+            coffeeName.innerHTML = `${menu[id].name}`;
+            coffeeVolume.innerHTML = `${menu[id].volume}`;
+            coffeePrice.innerHTML = `${menu[id].price}`;
             beauty.classList.toggle('is-flipped');
+            if (id === 0) { //Заказали эспрессо и можем кастомизировать
+                additiveChangers.forEach(changer => changer.style.display = 'block');
+                beautyFaceBack.style.justifyContent = 'flex-end';
+                beautyFaceBack.firstElementChild.style.marginRight = 'auto';
+            }
         }
         await setTimeout(() => {
             cards.forEach(card => {
@@ -132,28 +143,60 @@ cancelButton.addEventListener('click', () => {
         card.classList.toggle('inactive');
         card.firstElementChild.classList.toggle('card__face--front');
         card.firstElementChild.classList.toggle('card__face');
+        additiveChangers.forEach(changer => changer.style.display = 'none');
+        beautyFaceBack.style.justifyContent = 'space-around';
+        beautyFaceBack.firstElementChild.style.marginRight = '0';
         milkQuantityField.value = '0';
         syrupQuantityField.value = '0';
+        whatShouldIPrint();
         buttonWasClicked ? card.style.pointerEvents = 'none' : card.style.pointerEvents = 'auto';
     })
 });
+
+const whatShouldIPrint = () => {
+    if (+milkQuantityField.value !== 0) {
+        coffeeAdditiveMilk.innerHTML = (+syrupQuantityField.value === 0 ? ' с молоком' : ' с молоком и ');
+    } else {
+        coffeeAdditiveMilk.innerHTML = ''
+    }
+    switch (syrupQuantityField.value) {
+        case '1':
+            coffeeAdditiveSyrup.innerHTML = ' с одной порцией сиропа';
+            break;
+        case '2':
+            coffeeAdditiveSyrup.innerHTML = ' с двумя порциями сиропа';
+            break;
+        default:
+            coffeeAdditiveSyrup.innerHTML = '';
+    }
+};
 
 const milkChangeHandler = (value) => {
     let quantity = +milkQuantityField.value;
     if (value === '+') {
         milkQuantityField.value = `${++quantity}`;
+        coffeeVolume.innerHTML = +coffeeVolume.innerHTML + menu[6].volume;
+        coffeePrice.innerHTML = +coffeePrice.innerHTML + menu[6].price;
     } else if ((value === '-') && (quantity > 0)) {
         milkQuantityField.value = `${--quantity}`;
+        coffeeVolume.innerHTML = +coffeeVolume.innerHTML - menu[6].volume;
+        coffeePrice.innerHTML = +coffeePrice.innerHTML - menu[6].price;
     }
+    whatShouldIPrint();
 };
 
 const syrupChangeHandler = (value) => {
     let quantity = +syrupQuantityField.value;
     if ((value === '+') && (quantity < 2)) {
         syrupQuantityField.value = `${++quantity}`;
+        coffeeVolume.innerHTML = +coffeeVolume.innerHTML + menu[7].volume;
+        coffeePrice.innerHTML = +coffeePrice.innerHTML + menu[7].price;
     } else if ((value === '-') && (quantity > 0)) {
         syrupQuantityField.value = `${--quantity}`;
+        coffeeVolume.innerHTML = +coffeeVolume.innerHTML - menu[7].volume;
+        coffeePrice.innerHTML = +coffeePrice.innerHTML - menu[7].price;
     }
+    whatShouldIPrint();
 };
 
 milkPlusButton.addEventListener('click', () => milkChangeHandler(milkPlusButton.value));
